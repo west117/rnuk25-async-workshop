a1828f5aef42a5e43acfe2671591f598fa0f457f
David Silvester <david@west117.co.uk>
2025-02-19 10:14:36 +0000

    part 1

--- projects/01_actor/src/lib.rs
+++ projects/01_actor/src/lib.rs
@@ -1,22 +1,40 @@
-use std::{future::Future, marker::PhantomData};
+use std::future::Future;
 
-pub trait Actor: Send {
-    type Req;
-    type Reply;
+use anyhow::Context;
+use tokio::sync::{mpsc, oneshot};
+
+pub trait Actor: Send + 'static {
+    type Req: Send + 'static;
+    type Reply: Send + 'static;
 
     fn handle(&mut self, msg: Self::Req) -> impl Future<Output = Self::Reply> + Send;
 }
 
-// TODO: vvvvvv
+pub fn actor_spawn<A: Actor>(mut actor: A) -> MailboxRef<A> {
+    let (tx, mut rx) = mpsc::channel::<(A::Req, oneshot::Sender<A::Reply>)>(1);
+    tokio::spawn(async move {
+        loop {
+            let Some((msg, tx)) = rx.recv().await else {
+                break;
+            };
 
-pub fn actor_spawn<A: Actor>(_actor: A) -> MailboxRef<A> {
-    todo!()
+            let res = actor.handle(msg).await;
+            let _ = tx.send(res);
+        }
+    });
+
+    MailboxRef { tx }
 }
 
-pub struct MailboxRef<A: Actor>(PhantomData<A>);
+pub struct MailboxRef<A: Actor> {
+    tx: mpsc::Sender<(A::Req, oneshot::Sender<A::Reply>)>,
+}
 
 impl<A: Actor> MailboxRef<A> {
-    pub async fn ask(&self, _req: A::Req) -> anyhow::Result<A::Reply> {
-        todo!()
+    pub async fn ask(&self, req: A::Req) -> anyhow::Result<A::Reply> {
+        let (tx, rx) = oneshot::channel();
+        self.tx.send((req, tx)).await.unwrap();
+        let res = rx.await.context("")?;
+        Ok(res)
     }
 }
